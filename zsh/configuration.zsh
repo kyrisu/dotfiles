c37b644e645659d33c7cd201c9a448f8237eebf6
Krystian Paszkiewicz <krystian.paszkiewicz@gmail.com>
2014-02-23 01:22:10 +0100

    Added dwm config, zsh config and submodules

---
+++ zsh/configuration.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+# oh-my-zsh configuration
+export DISABLE_AUTO_TITLE="true"
+
+# Enable more syntax highlighting
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
+
+# CDPATH tells the cd command to look in
+# this colon-separated list of directories for your destination.
+CDPATH=$HOME:..:/etc
+
+# Disable flow control in rxvt, frees ctrl-s & ctrl-q mappings
+stty -ixon -ixoff
+
+# Add dircolors
+eval $(dircolors $DOTFILES/zsh/dircolors-solarized/dircolors.ansi-dark)
+
+# default text editor
+export EDITOR=vim
+
